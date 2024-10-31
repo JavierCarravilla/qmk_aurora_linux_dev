7540ff175d18dc4a15657106560491073bf69484
javier-carravilla <javier.carravilla@adevinta.com>
2024-10-31 16:17:41 +0100

    Add MacOs homerow

--- keymap.c
+++ keymap.c
@@ -15,12 +15,16 @@
  */
 
 #include QMK_KEYBOARD_H
+#if __has_include("keymap.h")
+#    include "keymap.h"
+#endif
 
 enum layer_names {
    _QUERTY,
+   _HOMEROW_MACOS,
    _LOWER,
    _RAISE,
-   _ADJUST,
+   _ADJUST
 };
 
 enum custom_keycodes {
@@ -73,12 +77,21 @@ tap_dance_action_t tap_dance_actions[] = {
 // Left-hand home rows mods
 #define LALT_KS LALT_T(KC_S)
 #define LSFT_KD LSFT_T(KC_D)
+//Linux
 #define LCTL_KF LCTL_T(KC_F)
+//MacOS
+#define LCTL_KA LCTL_T(KC_A)
+#define LGUI_KF LGUI_T(KC_F)
 
 // Right-hand home rows mods
 #define LALT_KL LALT_T(KC_L)
 #define RSFT_KK RSFT_T(KC_K)
+//Linux
 #define RCTL_KJ RCTL_T(KC_J)
+//MacOS
+#define RGUI_KJ RGUI_T(KC_J)
+#define RCTL_SC RCTL_T(KC_SCLN)
+
 
 //Tap dance alias
 #define PRT_SCR TD(TD_PRT_SCR)
@@ -96,23 +109,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  //|----------------------------|        |----------------------------|
    ),
 
+   [_HOMEROW_MACOS] = LAYOUT_split_3x6_3(
+        //|-----------------------------------------------------|        |------------------------------------------------------|
+            _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, _______,
+            _______, LCTL_KA, _______, _______, LGUI_KF, _______,         _______, RGUI_KJ, _______, _______, RCTL_SC, _______,
+            KC_LPAD, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, KC_MCTL,
+        //|-----------------------------------------------------|        |------------------------------------------------------|
+                                    _______, MO(_LOWER), _______,         _______, MO(_RAISE), _______
+                                 //|----------------------------|        |----------------------------|
+   ),
+
    [_LOWER] = LAYOUT_split_3x6_3(
         //|-----------------------------------------------------|        |-----------------------------------------------------|
-            QK_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,     KC_9,    KC_0, KC_BSPC,
-             KC_TAB, XXXXXXX, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX,         KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT, RFS_BWR, PRT_SCR,
-            KC_LGUI, DBG_EVA,   KC_F7,   KC_F8,   KC_F9, XXXXXXX,         KC_HOME, KC_PGDN, KC_PGUP,   KC_END, SEL_COL, KC_RSFT,
+            _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,     KC_9,    KC_0, KC_BSPC,
+            _______, _______, _______, _______, _______, XXXXXXX,         KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT, RFS_BWR, PRT_SCR,
+            _______, DBG_EVA,   KC_F7,   KC_F8,   KC_F9, XXXXXXX,         KC_HOME, KC_PGDN, KC_PGUP,   KC_END, SEL_COL, KC_RSFT,
         //|-----------------------------------------------------|        |-----------------------------------------------------|
-                                        KC_LSFT, _______, KC_SPC,          KC_ENT, MO(_ADJUST), KC_RALT
+                                       _______, _______, _______,         _______, MO(_ADJUST), _______
                                  //|----------------------------|        |----------------------------|
    ),
 
    [_RAISE] = LAYOUT_split_3x6_3(
         //|-----------------------------------------------------|        |-----------------------------------------------------|
-            QK_GESC, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-             KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
-            KC_LGUI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+            _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+            _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+            _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
         //|-----------------------------------------------------|        |-----------------------------------------------------|
-                                     KC_LSFT, MO(_ADJUST), KC_SPC,           KC_ENT, _______, KC_RALT
+                                     _______, MO(_ADJUST),_______,           _______, _______, _______
                                  //|----------------------------|        |----------------------------|
    ),
 
@@ -122,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
             RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         //|-----------------------------------------------------|        |-----------------------------------------------------|
-                                        KC_LSFT, _______, KC_SPC,           KC_ENT, _______, KC_RALT
+                                        XXXXXXX, _______,XXXXXXX,          XXXXXXX, _______, XXXXXXX
                                  //|----------------------------|        |----------------------------|
    )
 };
@@ -153,7 +176,6 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 };
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 
-
 //Overriding the delete key https://docs.qmk.fm/features/key_overrides#simple-example
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
@@ -162,6 +184,37 @@ const key_override_t *key_overrides[] = {
   &delete_key_override
 };
 
+// https://docs.qmk.fm/features/os_detection
+bool process_detected_host_os_kb(os_variant_t detected_os) {
+    if (!process_detected_host_os_user(detected_os)) {
+        return false;
+    }
+    switch (detected_os) {
+        case OS_MACOS:
+        case OS_IOS:
+            if (IS_LAYER_OFF(_HOMEROW_MACOS)) {
+              layer_on(_HOMEROW_MACOS);
+            }
+            break;
+        case OS_WINDOWS:
+            if (IS_LAYER_ON(_HOMEROW_MACOS)) {
+              layer_off(_HOMEROW_MACOS);
+            }
+            break;
+        case OS_LINUX:
+            if (IS_LAYER_ON(_HOMEROW_MACOS)) {
+              layer_off(_HOMEROW_MACOS);
+            }
+            break;
+        case OS_UNSURE:
+            break;
+    }
+    
+    return true;
+}
+
+
+>>>>>>> Stashed changes
 #ifdef RGBLIGHT_ENABLE
 void keyboard_post_init_user(void) {
   rgblight_enable_noeeprom(); // Enables RGB, without saving settings
