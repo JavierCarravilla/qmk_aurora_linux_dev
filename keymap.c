e28daab644e1e2e7f29c86355475f13f2ed522e1
Javier Carravilla <148545928+JavierCarravilla@users.noreply.github.com>
2024-10-31 15:45:42 +0100

    Remove the Null in the key_overrides array

--- keymap.c
+++ keymap.c
@@ -157,8 +157,7 @@ const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
-    &delete_key_override,
-    NULL // Null terminate the array of overrides!
+    &delete_key_override
 };
 
 
